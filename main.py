a6e242ee01a22ee54b17fc838364a259557507d2
Soebb <87156166+Soebb@users.noreply.github.com>
2022-01-17 04:26:06 +0330

    Update main.py

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ a1 = '1.mp3'
 a2 = '2.mp3'
 a3 = '3.mp3'
 a6 = '6.mp3'
-aac = 'a2.aac'
+
 main = folder.rsplit('/', 1)[1] + '\\'
 
 refresh_button = [
@@ -107,10 +107,11 @@ async def callback(bot, update):
     input = folder + "/" + update.data
     try:
         vname = update.data.replace('.ts', '.mp4')
+        aac = vname.rsplit(".", 1)[0]+'.aac'
         n = PTN.parse(vname.rsplit(".", 1)[0])
         title = n['title'].replace("-", " ")
         au2_1 = f'C:/All Projact Primer Pro/Audio Sound Serial Primer Pro Tag/{title}/2.1.mp3'
-        
+
         t2t = await update.message.reply_text('تایم واسه تگ صوت 2 (2.2 + 2.1) رو بفرست')
         t22: Message = await bot.listen(update.message.chat.id, filters=filters.text)
         t3t = await update.message.reply_text('پنج تا تایم واسه تگ صوت سوم رو بفرست\n3.mp3')
@@ -127,15 +128,15 @@ async def callback(bot, update):
         t6 = int(gettime(t66.text))
         prccs = await update.message.reply_text("processing..")
         os.system(f'ffmpeg -i "{au2_1}" -i 2.2.mp3 -y 2.mp3')
-        os.system(f'ffmpeg -i "{input}" -vn -i {a1} -vn -i {a2} -vn -i {a3} -vn -i {a6} -vn -filter_complex "[1]adelay=00000|00000[b]; [2]adelay={t2}|{t2}[c]; [3]adelay={t3_1}|{t3_1}[d]; [3]adelay={t3_2}|{t3_2}[e]; [3]adelay={t3_3}|{t3_3}[f]; [3]adelay={t3_4}|{t3_4}[g]; [3]adelay={t3_5}|{t3_5}[h]; [4]adelay={t6}|{t6}[i]; [0][b][c][d][e][f][g][h][i]amix=9" -c:a aac -b:a 125k -y {aac}')   
+        os.system(f'ffmpeg -i "{input}" -vn -i {a1} -vn -i {a2} -vn -i {a3} -vn -i {a6} -vn -filter_complex "[1]adelay=00000|00000[b]; [2]adelay={t2}|{t2}[c]; [3]adelay={t3_1}|{t3_1}[d]; [3]adelay={t3_2}|{t3_2}[e]; [3]adelay={t3_3}|{t3_3}[f]; [3]adelay={t3_4}|{t3_4}[g]; [3]adelay={t3_5}|{t3_5}[h]; [4]adelay={t6}|{t6}[i]; [0][b][c][d][e][f][g][h][i]amix=9" -c:a aac -b:a 125k -y "{aac}"')   
         time.sleep(10)
-        os.system(f'ffmpeg -i "{input}" -i {aac} -c copy -map 0:0 -map 1:0 -y "{tmp}{vname}"')
+        #os.system(f'ffmpeg -i "{input}" -i {aac} -c copy -map 0:0 -map 1:0 -y "{tmp}{vname}"')
         #await update.message.reply_text(f"Done. Check {tmp}{vname}")
-        await bot.send_video(chat_id=update.message.chat.id, video=tmp+vname, caption=f"also saved in {tmp}{vname}")
+        await bot.send_audio(chat_id=update.message.chat.id, audio=aac, caption=f"also saved in {tmp}{vname}")
         ask = await update.message.reply_text('remove output in system?\n /yes or /no')
         ans: Message = await bot.listen(update.message.chat.id, filters=filters.text)
         if "yes" in ans.text:
-            os.remove(tmp+vname)
+            os.remove(aac)
         await ans.delete(True)
         await ask.delete()
         await prccs.delete()
