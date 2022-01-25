f30204dab937da4e1a44215cd88d25a7ad785b19
Soebb <87156166+Soebb@users.noreply.github.com>
2022-01-25 23:21:32 +0330

    Update main.py

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ from hachoir.parser import createParser
 
 previous_cut_time = '02:00:04'
 
-
+folder = "C:/temp"
 BOT_TOKEN = " "
 API_ID = " "
 API_HASH = " "
 
-BOT_NAME = "cuttttter"
+BOT_NAME = "cuut"
 
 
 Bot = TelegramClient(BOT_NAME, API_ID, API_HASH).start(bot_token=BOT_TOKEN)
@@ -23,18 +23,20 @@ refresh_button = [
 ]
 msgid = 0
 chatid = 0
+v=folder+'*/'
+main =
 @Bot.on(events.NewMessage(incoming=True, func=lambda e: e.is_private))
 async def start(event):
     keyboard = []
     keyboard.append(refresh_button)
     try:
-        for file in glob.glob('C:/dlmacvin/1aa/*'):
+        for file in glob.glob(v):
             try:
                 keyboard.append(
                     [
                         Button.inline(
-                            file.rsplit('/', 1)[1].replace('1aa\\', ''),
-                            data=file.rsplit('/', 1)[1].replace('1aa\\', '')
+                            file.rsplit('/', 1)[1].replace(main, ''),
+                            data=file.rsplit('/', 1)[1].replace(main, '')
                         )
                     ]
                 )
@@ -55,12 +57,12 @@ async def callback(event):
         keyboard = []
         keyboard.append(refresh_button)
         try:
-            for file in glob.glob('C:/dlmacvin/1aa/*'):
+            for file in glob.glob(v):
                 keyboard.append(
                     [
                         Button.inline(
-                            file.rsplit('/', 1)[1].replace('1aa\\', ''),
-                            data=file.rsplit('/', 1)[1].replace('1aa\\', '')
+                            file.rsplit('/', 1)[1].replace(main, ''),
+                            data=file.rsplit('/', 1)[1].replace(main, '')
                         )
                     ]
                 )
@@ -75,7 +77,7 @@ async def callback(event):
         return
     try:
         name = event.data.decode('utf-8')
-        input = 'C:/dlmacvin/1aa/' + name
+        input = folder + "/" + name
         metadata = extractMetadata(createParser(input))
         duration = int(metadata.get('duration').seconds)
         dtime = str(datetime.timedelta(seconds=duration))[:11]
@@ -97,13 +99,7 @@ async def callback(event):
         ext = '.' + name.rsplit('.', 1)[1]
         end_sec = sum(x * int(t) for x, t in zip([1, 60, 3600], reversed(end.split(":"))))
         os.system(f'''ffmpeg -ss {start} -i "{input}" -to {end} -c copy "C:/dlmacvin/1aa/videos/{name.replace(ext, '-0'+ext)}"''')
-        cut_steps = []
-        dif = duration - int(end_sec)
-        for i in range(dif // 10):
-            cut_steps.append(i * 10)
-        for step in cut_steps:
-            stp = str(end_sec + step)
-            os.system(f'''ffmpeg -ss {start} -i "{input}" -to {stp} -c copy "C:/dlmacvin/1aa/videos/{name.replace(ext, '-'+str(step/10)+ext)}"''')
+
         await process_msg.delete()
         if chatid == 0:
             msg = await Bot.send_message(event.chat_id, 'Done! ' + name)
