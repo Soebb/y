ff79e00d3c627854f23ffd3f6aee61fdf500c1b5
Soebb <87156166+Soebb@users.noreply.github.com>
2022-02-13 01:06:36 +0330

    Update D.py

--- D.py
+++ D.py
@@ -36,27 +36,26 @@ class DaftsexIE(InfoExtractor):
         title = self._search_regex(
             r'<meta.*itemprop ?= ?"name".*content ?= ?"([^"]+)".*/>',
             webpage, 'Title', default='Empty Title', fatal=False)
-        self.write_debug('title: %s' % title)
+
         uploadDate = self._search_regex(
             r'<meta.*itemprop ?= ?"uploadDate".*content ?= ?"([^"]+)".*/?>',
             webpage, 'Upload Date', fatal=False)
-        self.write_debug('Upload Date: %s' % uploadDate)
+
         timestamp = unified_timestamp(uploadDate)
-        self.write_debug('timestamp: %s' % timestamp)
+
         description = self._search_regex(
             r'<meta.*itemprop ?= ?"description".*content ?= ?"([^"]+)".*/>',
             webpage, 'Description', fatal=False)
-        self.write_debug('description: %s' % description)
 
         globalEmbed_url = self._search_regex(
             r'<script.+?window.globEmbedUrl = \'((?:https?:)?//(?:daxab\.com|dxb\.to|[^/]+/player)/[^\']+)\'.*?></script>',
             webpage, 'global Embed url', flags=re.DOTALL)
+
         hash = self._search_regex(
             r'<script id="data-embed-video.+?hash: "([^"]+)"[^<]*</script>',
             webpage, 'Hash', flags=re.DOTALL)
 
         embed_url = globalEmbed_url + hash
-        self.write_debug('embed_url: %s' % embed_url)
 
         if VKIE.suitable(embed_url):
             return self.url_result(embed_url, VKIE.ie_key(), video_id)
@@ -67,10 +66,9 @@ class DaftsexIE(InfoExtractor):
         globParams = self._parse_json(self._search_regex(
             r'<script id="globParams">.*window.globParams = ([^;]+);[^<]+</script>',
             embed_page, 'Global Parameters', flags=re.DOTALL), video_id, transform_source=js_to_json)
-        self.write_debug('globParams: %s' % globParams)
+
         hostName = compat_b64decode(globParams['server'][::-1]).decode()
         server = 'https://%s/method/video.get/' % hostName
-        self.write_debug('server: %s' % server)
 
         item = self._download_json(
             server + video_id, video_id,
@@ -78,7 +76,7 @@ class DaftsexIE(InfoExtractor):
                 'token': globParams['video']['access_token'],
                 'videos': video_id,
                 'ckey': globParams['c_key'],
-                #'credentials': globParams['video']['credentials'],
+                'credentials': globParams['video']['credentials'],
             })['response']['items'][0]
 
         formats = []
@@ -86,15 +84,14 @@ class DaftsexIE(InfoExtractor):
             if f_id == 'external':
                 return self.url_result(f_url)
             ext, height = f_id.split('_')
-            if globParams['video']['partial']['quality'].get(height) is not None:
+            if globParams['video']['partial']['quality']['height'] is not None:
                 formats.append({
-                    'format_id': height + 'p',
-                    'url': f_url.replace('https://', 'https://%s/' % hostName) + '&videos=%s' % video_id + '&extra_key=%s' % globParams['video']['partial']['quality'][height],
+                    'format_id': f'{height}p',
+                    'url': f'https://{hostName}/{f_url[8:]}&videos={video_id}&extra_key={globParams["video"]["partial"]["quality"][height]}',
                     'height': int_or_none(height),
                     'ext': ext,
                 })
         self._sort_formats(formats)
-        self.write_debug('formats: %s' % formats)
 
         thumbnails = []
         for k, v in item.items():
