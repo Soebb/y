9b5fb9e0c550d34ff0e4c0c8942853fb48ab4ad3
Soebb <87156166+Soebb@users.noreply.github.com>
2022-02-13 04:15:46 +0330

    Update D.py

--- D.py
+++ D.py
@@ -46,86 +46,7 @@ class DaftsexIE(InfoExtractor):
         video_id = self._match_id(url)
         webpage = self._download_webpage(url, video_id)
 
-        globParams = self._parse_json(self._search_regex(
-            r'<script id="globParams">.*window.globParams = ([^;]+);[^<]+</script>',
-            embed_page, 'Global Parameters', flags=re.DOTALL), video_id, transform_source=js_to_json)
-
-        if 'credentials' in globParams['video']:
-            title = self._search_regex(
-                r'<meta.*itemprop ?= ?"name".*content ?= ?"([^"]+)".*/>',
-                webpage, 'Title', default='Empty Title', fatal=False)
-            uploadDate = self._search_regex(
-                r'<meta.*itemprop ?= ?"uploadDate".*content ?= ?"([^"]+)".*/?>',
-                webpage, 'Upload Date', fatal=False)
-            timestamp = unified_timestamp(uploadDate)
-            description = self._search_regex(
-                r'<meta.*itemprop ?= ?"description".*content ?= ?"([^"]+)".*/>',
-                webpage, 'Description', fatal=False)
-
-            globalEmbed_url = self._search_regex(
-                r'<script.+?window.globEmbedUrl = \'((?:https?:)?//(?:daxab\.com|dxb\.to|[^/]+/player)/[^\']+)\'.*?></script>',
-                webpage, 'global Embed url', flags=re.DOTALL)
-            hash = self._search_regex(
-                r'<script id="data-embed-video.+?hash: "([^"]+)"[^<]*</script>',
-                webpage, 'Hash', flags=re.DOTALL)
-
-            embed_url = globalEmbed_url + hash
-
-            if VKIE.suitable(embed_url):
-                return self.url_result(embed_url, VKIE.ie_key(), video_id)
-
-            embed_page = self._download_webpage(
-                embed_url, video_id, 'Downloading embed webpage', headers={'Referer': url})
-
-            hostName = compat_b64decode(globParams['server'][::-1]).decode()
-            server = 'https://%s/method/video.get/' % hostName
-
-            item = self._download_json(
-                f'{server}{video_id}', video_id,
-                headers={'Referer': url}, query={
-                    'token': globParams['video']['access_token'],
-                    'videos': video_id,
-                    'ckey': globParams['c_key'],
-                    'credentials': globParams['video']['credentials'],
-                })['response']['items'][0]
-
-            formats = []
-            for f_id, f_url in item.get('files', {}).items():
-                if f_id == 'external':
-                    return self.url_result(f_url)
-                ext, height = f_id.split('_')
-                if globParams['video']['partial']['quality']['height'] is not None:
-                    formats.append({
-                        'format_id': f'{height}p',
-                        'url': f'https://{hostName}/{f_url[8:]}&videos={video_id}&extra_key={globParams["video"]["partial"]["quality"][height]}',
-                        'height': int_or_none(height),
-                        'ext': ext,
-                    })
-            self._sort_formats(formats)
-
-            thumbnails = []
-            for k, v in item.items():
-                if k.startswith('photo_') and v:
-                    width = k.replace('photo_', '')
-                    thumbnails.append({
-                        'id': width,
-                        'url': v,
-                        'width': int_or_none(width),
-                    })
-
-            return {
-                'id': video_id,
-                'title': title,
-                'formats': formats,
-                'comment_count': int_or_none(item.get('comments')),
-                'description': description,
-                'duration': int_or_none(item.get('duration')),
-                'thumbnails': thumbnails,
-                'timestamp': timestamp,
-                'view_count': int_or_none(item.get('views')),
-            }
-
-        elif not 'credentials' in globParams['video']:
+        try:
             title = get_elements_by_class('heading', webpage)[-1]
             duration = parse_duration(self._search_regex(
                 r'Duration: ((?:[0-9]{2}:){0,2}[0-9]{2})',
@@ -175,4 +96,83 @@ class DaftsexIE(InfoExtractor):
                 'thumbnail': thumbnail,
                 'view_count': views,
                 'age_limit': 18,
+            }
+
+        except KeyError:
+            title = self._search_regex(
+                r'<meta.*itemprop ?= ?"name".*content ?= ?"([^"]+)".*/>',
+                webpage, 'Title', default='Empty Title', fatal=False)
+            uploadDate = self._search_regex(
+                r'<meta.*itemprop ?= ?"uploadDate".*content ?= ?"([^"]+)".*/?>',
+                webpage, 'Upload Date', fatal=False)
+            timestamp = unified_timestamp(uploadDate)
+            description = self._search_regex(
+                r'<meta.*itemprop ?= ?"description".*content ?= ?"([^"]+)".*/>',
+                webpage, 'Description', fatal=False)
+
+            globalEmbed_url = self._search_regex(
+                r'<script.+?window.globEmbedUrl = \'((?:https?:)?//(?:daxab\.com|dxb\.to|[^/]+/player)/[^\']+)\'.*?></script>',
+                webpage, 'global Embed url', flags=re.DOTALL)
+            hash = self._search_regex(
+                r'<script id="data-embed-video.+?hash: "([^"]+)"[^<]*</script>',
+                webpage, 'Hash', flags=re.DOTALL)
+
+            embed_url = globalEmbed_url + hash
+
+            if VKIE.suitable(embed_url):
+                return self.url_result(embed_url, VKIE.ie_key(), video_id)
+
+            embed_page = self._download_webpage(
+                embed_url, video_id, 'Downloading embed webpage', headers={'Referer': url})
+
+            globParams = self._parse_json(self._search_regex(
+                r'<script id="globParams">.*window.globParams = ([^;]+);[^<]+</script>',
+                embed_page, 'Global Parameters', flags=re.DOTALL), video_id, transform_source=js_to_json)
+
+            hostName = compat_b64decode(globParams['server'][::-1]).decode()
+            server = 'https://%s/method/video.get/' % hostName
+
+            item = self._download_json(
+                f'{server}{video_id}', video_id,
+                headers={'Referer': url}, query={
+                    'token': globParams['video']['access_token'],
+                    'videos': video_id,
+                    'ckey': globParams['c_key'],
+                    'credentials': globParams['video']['credentials'],
+                })['response']['items'][0]
+
+            formats = []
+            for f_id, f_url in item.get('files', {}).items():
+                if f_id == 'external':
+                    return self.url_result(f_url)
+                ext, height = f_id.split('_')
+                if globParams['video']['partial']['quality']['height'] is not None:
+                    formats.append({
+                        'format_id': f'{height}p',
+                        'url': f'https://{hostName}/{f_url[8:]}&videos={video_id}&extra_key={globParams["video"]["partial"]["quality"][height]}',
+                        'height': int_or_none(height),
+                        'ext': ext,
+                    })
+            self._sort_formats(formats)
+
+            thumbnails = []
+            for k, v in item.items():
+                if k.startswith('photo_') and v:
+                    width = k.replace('photo_', '')
+                    thumbnails.append({
+                        'id': width,
+                        'url': v,
+                        'width': int_or_none(width),
+                    })
+
+            return {
+                'id': video_id,
+                'title': title,
+                'formats': formats,
+                'comment_count': int_or_none(item.get('comments')),
+                'description': description,
+                'duration': int_or_none(item.get('duration')),
+                'thumbnails': thumbnails,
+                'timestamp': timestamp,
+                'view_count': int_or_none(item.get('views')),
             }
